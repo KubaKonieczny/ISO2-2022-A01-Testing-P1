7929bca454a939fecef6911e15fa8877a7ca77dc
jakub <jakub.konieczny@alu.uclm.es>
2022-12-20 23:05:05 +0100

    initial

---
+++ Testing.P1/src/main/java/SoftwareEngineering/A1/Testing/P1/App.java
@@ -0,0 +1,15 @@
+package SoftwareEngineering.A1.Testing.P1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Date date = new Date(1,1,1);
+    	
+        System.out.println( date.isLeap() );
+    }
+}
